95d5776af9f467ef688ec29fb4b8300b999666a0
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-02-18 18:55:31 +0700

    fix: code qualities issues by PMD because of unnecessary public modifier on interface

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,9 +4,9 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
-    public Product getProduct(String productId);
-    public Product updateProduct(Product product);
-    public void deleteProduct(String productId);
+    Product create(Product product);
+    List<Product> findAll();
+    Product getProduct(String productId);
+    Product updateProduct(Product product);
+    void deleteProduct(String productId);
 }
