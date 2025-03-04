17c18503cebcf6a7b0415dc4d6e7956f7c3ae947
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-03-04 14:11:16 +0700

    [GREEN] Implement PaymentByVoucher sub-feature

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentByVoucher.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentByVoucher.java
@@ -11,11 +11,38 @@ public class PaymentByVoucher extends Payment {
 
     @Override
     public void setPaymentData(Map<String, String> paymentData) {
+        if (paymentData == null || !paymentData.containsKey("voucherCode") || paymentData.get("voucherCode") == null) {
+            throw new IllegalArgumentException();
+        }
+
+        super.setPaymentData(paymentData);
+        setStatus(validateVoucher(paymentData));
     }
 
     private static String validateVoucher(Map<String, String> paymentData) {
+        String voucherCode = paymentData.get("voucherCode");
+        if (!isValidVoucher(voucherCode)) {
+            return PaymentStatus.REJECTED.getValue();
+        }
+        return PaymentStatus.SUCCESS.getValue();
     }
 
     private static boolean isValidVoucher(String voucherCode) {
+        if (voucherCode == null || voucherCode.length() != 16) {
+            return false;
+        }
+
+        if (!voucherCode.startsWith("ESHOP")) {
+            return false;
+        }
+
+        int digitCount = 0;
+        for (char c : voucherCode.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                digitCount++;
+            }
+        }
+
+        return digitCount == 8;
     }
 }
