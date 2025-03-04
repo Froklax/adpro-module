6827446943882613c7a56f9f3c5ad1a628dbcd20
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2025-03-04 14:04:02 +0700

    [REFACTOR] Add setter to Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -3,11 +3,13 @@ package id.ac.ui.cs.advprog.eshop.model;
 import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
 import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Arrays;
 import java.util.Map;
 
 @Getter
+@Setter
 public class Payment {
     private String id;
     private String method;
